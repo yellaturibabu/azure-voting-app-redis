84c2822285f2a204de8457aed129a2a69f3c78b4
yellaturibabu <yellaturibabu@gmail.com>
2019-09-11 20:10:10 +0530

    Update Deploy.groovy

--- dev/config/Deploy.groovy
+++ dev/config/Deploy.groovy
@@ -3,4 +3,6 @@ subscriptionID = '92127454-cf8d-446b-a186-dff7862624b9'
 resourceGroup = 'aks-resource-group'
 clusterName = 'aks-cluster'
 namespace = 'default'
-release  = 'azure-voting-app-redis'
+layout_release  = 'azure-voting-app-redis'
+layout_chart = 'stable/tomcat'
+
